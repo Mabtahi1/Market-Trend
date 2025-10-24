221ab108071c72caff5b4fd27eb9c0242e5a6927
Mehdi Abtahi <36539571+Mabtahi1@users.noreply.github.com>
2025-10-24 14:16:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2006,4 +2006,4 @@ if __name__ == '__main__':
     print(f"Web scraping available: {WEB_SCRAPING_AVAILABLE}")
     
     port = int(os.environ.get('PORT', 5002))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
